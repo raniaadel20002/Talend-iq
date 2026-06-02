8ebf10f52bf854d6ebecafb406370807b56da510
raniaadel <raniaadel20002@gmail.com>
2026-06-02 20:35:10 +0300

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { ENV } from "./lib/env.js"; 
+
+
+const app = express();
+
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: "success from api"})
+});
+
+
+
+app.listen(ENV.PORT, () => {
+    console.log('Server is running on port:', ENV.PORT);
+});

945c7bfdf7f7c9c8c2fa7231f871e5eec8f69ab7
raniaadel <raniaadel20002@gmail.com>
2026-06-03 22:39:16 +0300

    db and tools setup

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,7 +5,7 @@ import { ENV } from "./env.js";
 export const connectDB = async () => {
     try {
         await mongoose.connect(ENV.DB_URL);
-        console.log("Connected to MongoDB");
+        console.log("✅ Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     }
--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ const startServer = async () => {
     try {
         await connectDB();
         app.listen(ENV.PORT, () => {
-            console.log('Server is running on port:', ENV.PORT);
+            console.log('Server is running on port',ENV.PORT+"...");
         });
     } catch (error) {
         console.error('Error starting the server', error);
